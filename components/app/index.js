3aed397556ed14bff6b25e5b7ab84b45dd73dce1
KCraw <kristopher.craw@gmail.com>
2016-02-16 01:05:10 -0600

    No need for Dimensions with flexbox

--- components/app/index.js
+++ components/app/index.js
@@ -15,19 +15,13 @@
 
 import React, {Component, View} from 'react-native';
 import _ from 'lodash';
-import Dimensions from 'Dimensions';
 
 import {data} from '../../data/mock.json';
-import * as styles from './styles';
+import styles from './styles';
 
 import RecommendationScene from '../recommendation-scene';
 
 export default class Gustave extends Component {
-
-  constructor(props){
-    super(props);
-    this.styles = styles.create(Dimensions.get('window'));
-  }
 
   static defaultProps = {
     recs: data
@@ -39,7 +33,7 @@ export default class Gustave extends Component {
 
   render() {
     return (
-      <RecommendationScene recs={this.props.recs} style={this.styles.scene} onCommit={this.onCommit.bind(this)} />
+      <RecommendationScene recs={this.props.recs} style={styles.scene} onCommit={this.onCommit.bind(this)} />
     );
   }
 }
--- components/app/styles.js
+++ components/app/styles.js
@@ -2,13 +2,10 @@
 
 import React, {StyleSheet} from 'react-native';
 
-export var create = function(dimensions) {
-  return StyleSheet.create({
-    scene: {
-      width: dimensions.width,
-      height: dimensions.height - 20,
-      backgroundColor: '#cccccc',
-      marginTop: 20,
-    },
-  });
-}
+export default StyleSheet.create({
+  scene: {
+    flex: 1,
+    backgroundColor: '#cccccc',
+    marginTop: 20,
+  },
+});
