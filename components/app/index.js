e071caba9e77225b097e4bcd71f846f9cdf2f5f1
KCraw <kristopher.craw@gmail.com>
2016-02-16 18:01:00 -0600

    Navigator setup done

--- components/app/index.js
+++ components/app/index.js
@@ -13,7 +13,7 @@
 
 */
 
-import React, {Component, View} from 'react-native';
+import React, {Component, View, Navigator} from 'react-native';
 import _ from 'lodash';
 
 import {data} from '../../data/mock.json';
@@ -33,7 +33,24 @@ export default class Gustave extends Component {
 
   render() {
     return (
-      <RecommendationScene recs={this.props.recs} style={styles.scene} onCommit={this.onCommit.bind(this)} />
+      <Navigator 
+        style={styles.scene}
+        initialRoute={{id: 'rec'}}
+        renderScene={this._navigatorRenderScene.bind(this)} />
     );
   }
+
+  _navigatorRenderScene(route, navigator) {
+    switch(route.id) {
+      case 'rec':
+        return (
+          <RecommendationScene navigator={navigator} recs={this.props.recs} onCommit={this.onCommit.bind(this)} />
+        );
+    }
+  }
+  // render() {
+  //   return (
+  //     <RecommendationScene recs={this.props.recs} style={styles.scene} onCommit={this.onCommit.bind(this)} />
+  //   );
+  // }
 }
